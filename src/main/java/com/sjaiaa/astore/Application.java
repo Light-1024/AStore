b9688e7e1a7c4e21aea8b76b1ee0e1f2425f5983
sjaiaa <2471684956@qq.com>
2023-03-11 21:04:58 +0800

    第一次提交

---
+++ src/main/java/com/sjaiaa/astore/Application.java
@@ -0,0 +1,16 @@
+package com.sjaiaa.astore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sjaiaa
+ * @date 2023/3/11 20:55
+ * @discription
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
